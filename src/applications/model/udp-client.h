shreyasgowda63/NS_3

applications: Combine RemoteAddress and RemotePort attributes to a single one in UdpClient
Proposed by Sharan Naribole

src/applications/model/udp-client.h
@@ -12,10 +12,13 @@
 #define UDP_CLIENT_H
 
 #include "ns3/application.h"
+#include "ns3/deprecated.h"
 #include "ns3/event-id.h"
 #include "ns3/ipv4-address.h"
 #include "ns3/ptr.h"
 #include <ns3/traced-callback.h>
+
+#include <optional>
 
 namespace ns3
 {
@@ -43,17 +46,22 @@ class UdpClient : public Application
 
     ~UdpClient() override;
 
+    static constexpr uint16_t DEFAULT_PORT{100}; //!< default port
+
     /**
      * \brief set the remote address and port
      * \param ip remote IP address
      * \param port remote port
+     * \deprecated Deprecated since ns-3.43. Use SetRemote without port parameter instead.
      */
-    void SetRemote(Address ip, uint16_t port);
+    NS_DEPRECATED_3_43("Use SetRemote without port parameter instead")
+    void SetRemote(const Address& ip, uint16_t port);
+
     /**
      * \brief set the remote address
      * \param addr remote address
      */
-    void SetRemote(Address addr);
+    void SetRemote(const Address& addr);
 
     /**
      * \return the total bytes sent by this app
@@ -69,6 +77,24 @@ class UdpClient : public Application
      */
     void Send();
 
+    /**
+     * \brief Set the remote port (temporary function until deprecated attributes are removed)
+     * \param port remote port
+     */
+    void SetPort(uint16_t port);
+
+    /**
+     * \brief Get the remote port (temporary function until deprecated attributes are removed)
+     * \return the remote port
+     */
+    uint16_t GetPort() const;
+
+    /**
+     * \brief Get the remote address (temporary function until deprecated attributes are removed)
+     * \return the remote address
+     */
+    Address GetRemote() const;
+
     /// Traced Callback: transmitted packets.
     TracedCallback<Ptr<const Packet>> m_txTrace;
 
@@ -79,17 +105,17 @@ class UdpClient : public Application
     Time m_interval;  //!< Packet inter-send time
     uint32_t m_size;  //!< Size of the sent packet (including the SeqTsHeader)
 
-    uint32_t m_sent;       //!< Counter for sent packets
-    uint64_t m_totalTx;    //!< Total bytes sent
-    Ptr<Socket> m_socket;  //!< Socket
-    Address m_peerAddress; //!< Remote peer address
-    uint16_t m_peerPort;   //!< Remote peer port
-    uint8_t m_tos;         //!< The packets Type of Service
-    EventId m_sendEvent;   //!< Event to send the next packet
+    uint32_t m_sent;                    //!< Counter for sent packets
+    uint64_t m_totalTx;                 //!< Total bytes sent
+    Ptr<Socket> m_socket;               //!< Socket
+    Address m_peer;                     //!< Peer address
+    std::optional<uint16_t> m_peerPort; //!< Remote peer port (deprecated) // NS_DEPRECATED_3_43
+    uint8_t m_tos;                      //!< The packets Type of Service
+    EventId m_sendEvent;                //!< Event to send the next packet
 
 #ifdef NS3_LOG_ENABLE
-    std::string m_peerAddressString; //!< Remote peer address string
-#endif                               // NS3_LOG_ENABLE
+    std::string m_peerString; //!< Remote peer address string
+#endif                        // NS3_LOG_ENABLE
 };
 
 } // namespace ns3

404SkillNotFound/ns-3-dev

src/applications/model/udp-client.h
@@ -12,10 +12,13 @@
 #define UDP_CLIENT_H
 
 #include "ns3/application.h"
+#include "ns3/deprecated.h"
 #include "ns3/event-id.h"
 #include "ns3/ipv4-address.h"
 #include "ns3/ptr.h"
 #include <ns3/traced-callback.h>
+
+#include <optional>
 
 namespace ns3
 {
@@ -40,20 +43,23 @@ class UdpClient : public Application
     static TypeId GetTypeId();
 
     UdpClient();
-
     ~UdpClient() override;
+
+    static constexpr uint16_t DEFAULT_PORT{100}; //!< default port
 
     /**
      * \brief set the remote address and port
      * \param ip remote IP address
      * \param port remote port
      */
-    void SetRemote(Address ip, uint16_t port);
+    NS_DEPRECATED_3_44("Use SetRemote without port parameter instead")
+    void SetRemote(const Address& ip, uint16_t port);
+
     /**
      * \brief set the remote address
      * \param addr remote address
      */
-    void SetRemote(Address addr);
+    void SetRemote(const Address& addr);
 
     /**
      * \return the total bytes sent by this app
@@ -69,6 +75,24 @@ class UdpClient : public Application
      */
     void Send();
 
+    /**
+     * \brief Set the remote port (temporary function until deprecated attributes are removed)
+     * \param port remote port
+     */
+    void SetPort(uint16_t port);
+
+    /**
+     * \brief Get the remote port (temporary function until deprecated attributes are removed)
+     * \return the remote port
+     */
+    uint16_t GetPort() const;
+
+    /**
+     * \brief Get the remote address (temporary function until deprecated attributes are removed)
+     * \return the remote address
+     */
+    Address GetRemote() const;
+
     /// Traced Callback: transmitted packets.
     TracedCallback<Ptr<const Packet>> m_txTrace;
 
@@ -79,17 +103,17 @@ class UdpClient : public Application
     Time m_interval;  //!< Packet inter-send time
     uint32_t m_size;  //!< Size of the sent packet (including the SeqTsHeader)
 
-    uint32_t m_sent;       //!< Counter for sent packets
-    uint64_t m_totalTx;    //!< Total bytes sent
-    Ptr<Socket> m_socket;  //!< Socket
-    Address m_peerAddress; //!< Remote peer address
-    uint16_t m_peerPort;   //!< Remote peer port
-    uint8_t m_tos;         //!< The packets Type of Service
-    EventId m_sendEvent;   //!< Event to send the next packet
+    uint32_t m_sent;                    //!< Counter for sent packets
+    uint64_t m_totalTx;                 //!< Total bytes sent
+    Ptr<Socket> m_socket;               //!< Socket
+    Address m_peer;                     //!< Peer address
+    std::optional<uint16_t> m_peerPort; //!< Remote peer port (deprecated) // NS_DEPRECATED_3_44
+    uint8_t m_tos;                      //!< The packets Type of Service
+    EventId m_sendEvent;                //!< Event to send the next packet
 
 #ifdef NS3_LOG_ENABLE
-    std::string m_peerAddressString; //!< Remote peer address string
-#endif                               // NS3_LOG_ENABLE
+    std::string m_peerString; //!< Remote peer address string
+#endif                        // NS3_LOG_ENABLE
 };
 
 } // namespace ns3