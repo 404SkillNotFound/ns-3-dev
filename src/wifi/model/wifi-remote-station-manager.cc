nsnam/ns-3-dev-git

wifi: Add support for 20MHz only operations in 5 GHz and 6 GHz bands for 11ax and 11be

src/wifi/model/wifi-remote-station-manager.cc
@@ -1712,6 +1712,10 @@ WifiRemoteStationManager::AddStationHeCapabilities(Mac48Address from,
         {
             state->m_channelWidth = MHz_t{80};
         }
+        else if (heCapabilities.GetChannelWidthSet() == 0x00)
+        {
+            state->m_channelWidth = MHz_t{20};
+        }
         // For other cases at 5 GHz, the supported channel width is set by the VHT capabilities
     }
     else if (m_wifiPhy->GetPhyBand() == WIFI_PHY_BAND_2_4GHZ)

404SkillNotFound/ns-3-dev

src/wifi/model/wifi-remote-station-manager.cc
@@ -1684,6 +1684,10 @@ WifiRemoteStationManager::AddStationHeCapabilities(Mac48Address from,
         {
             state->m_channelWidth = MHz_u{80};
         }
+        else if (heCapabilities.GetChannelWidthSet() == 0x00)
+        {
+            state->m_channelWidth = MHz_u{20};
+        }
         // For other cases at 5 GHz, the supported channel width is set by the VHT capabilities
     }
     else if (m_wifiPhy->GetPhyBand() == WIFI_PHY_BAND_2_4GHZ)