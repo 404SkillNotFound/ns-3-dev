nsnam/ns-3-dev-git

wifi: Use EHT RUs for EHT PPDUs

src/wifi/model/eht/eht-ppdu.h
@@ -151,6 +151,12 @@ class EhtPpdu : public HePpdu
                                     uint8_t ehtPpduType,
                                     std::optional<bool> isLow80MHz);
 
+  protected:
+    WifiRu::RuSpec GetRuSpec(std::size_t ruAllocIndex,
+                             MHz_t bw,
+                             RuType ruType,
+                             std::size_t phyIndex) const override;
+
   private:
     bool IsDlMu() const override;
     bool IsUlMu() const override;

src/wifi/model/he/he-phy.h
@@ -427,29 +427,30 @@ class HePhy : public VhtPhy
      */
     static Time GetSymbolDuration(Time guardInterval);
 
+    /// structure containing the information about the RU subcarriers to be able to converted to the
+    /// indices used by the Spectrum model
+    struct RuSubcarriersInfo
+    {
+        MHz_t bandWidth; ///< width of the band used for the OFDMA transmission. Must be a multiple
+                         ///< of 20 MHz
+        MHz_t guardBandwidth;                        ///< width of the guard band
+        const std::vector<MHz_t>& centerFrequencies; ///< center frequency of each segment
+        MHz_t totalWidth;                            ///< width of the operating channel
+        Hz_t subcarrierSpacing;                      ///< subcarrier spacing
+        WifiModulationClass mc;                      ///< modulation class used for the transmission
+        SubcarrierRange subcarrierRange;             ///< subcarrier range of the RU
+        uint8_t bandIndex{0}; ///< index (starting at 0) of the band within the operating channel
+    };
+
     /**
-     * @param bandWidth the width of the band used for the OFDMA transmission. Must be a multiple of
-     * 20 MHz
-     * @param guardBandwidth width of the guard band
-     * @param centerFrequencies the center frequency of each segment
-     * @param totalWidth the width of the operating channel
-     * @param subcarrierSpacing the subcarrier spacing
-     * @param subcarrierRange the subcarrier range of the RU
-     * @param bandIndex the index (starting at 0) of the band within the operating channel
-     * @return the converted subcarriers
-     *
      * This is a helper function to convert RU subcarriers, which are relative to the center
-     * frequency subcarrier, to the indexes used by the Spectrum model. The size of the returned
+     * frequency subcarrier, to the indices used by the Spectrum model. The size of the returned
      * vector corresponds to the number of segments covered by the RU.
+     *
+     * @param info the information about the RU subcarrier to convert
+     * @return the indices used by the Spectrum model for the provided RU subcarrier
      */
-    static std::vector<WifiSpectrumBandIndices> ConvertRuSubcarriers(
-        MHz_t bandWidth,
-        MHz_t guardBandwidth,
-        const std::vector<MHz_t>& centerFrequencies,
-        MHz_t totalWidth,
-        Hz_t subcarrierSpacing,
-        SubcarrierRange subcarrierRange,
-        uint8_t bandIndex = 0);
+    static std::vector<WifiSpectrumBandIndices> ConvertRuSubcarriers(const RuSubcarriersInfo& info);
 
   protected:
     PhyFieldRxStatus ProcessSig(Ptr<Event> event,

src/wifi/model/wifi-tx-vector.cc
@@ -691,6 +691,7 @@ WifiTxVector::GetInactiveSubchannels() const
 void
 WifiTxVector::SetCenter26ToneRuIndication(Center26ToneRuIndication center26ToneRuIndication)
 {
+    NS_ASSERT(GetModulationClass() == WIFI_MOD_CLASS_HE);
     if (IsDlMu())
     {
         NS_ASSERT(center26ToneRuIndication == DeriveCenter26ToneRuIndication());
@@ -701,7 +702,7 @@ WifiTxVector::SetCenter26ToneRuIndication(Center26ToneRuIndication center26ToneR
 std::optional<Center26ToneRuIndication>
 WifiTxVector::GetCenter26ToneRuIndication() const
 {
-    if (!IsDlMu() || (m_channelWidth < MHz_t{80}))
+    if ((GetModulationClass() != WIFI_MOD_CLASS_HE) || !IsDlMu() || (m_channelWidth < MHz_t{80}))
     {
         return std::nullopt;
     }
@@ -801,7 +802,10 @@ WifiTxVector::GetUserInfoMapOrderedByRus(uint8_t p20Index) const
 RuAllocation
 WifiTxVector::DeriveRuAllocation(uint8_t p20Index) const
 {
-    RuAllocation ruAllocations(Count20MHzSubchannels(m_channelWidth), EMPTY_242_TONE_HE_RU);
+    const auto mc = GetModulationClass();
+    const auto emptyRu =
+        (mc == WIFI_MOD_CLASS_HE) ? EMPTY_242_TONE_HE_RU : UNASSIGNED_242_TONE_EHT_RU;
+    RuAllocation ruAllocations(Count20MHzSubchannels(m_channelWidth), emptyRu);
     std::vector<RuType> ruTypes{};
     ruTypes.resize(ruAllocations.size());
     const auto& orderedMap = GetUserInfoMapOrderedByRus(p20Index);
@@ -817,8 +821,6 @@ WifiTxVector::DeriveRuAllocation(uint8_t p20Index) const
             continue;
         }
         const auto ruBw = WifiRu::GetBandwidth(ruType);
-        NS_ASSERT_MSG(WifiRu::IsHe(ru), "EHT RUs should not be used yet");
-        const auto mc{WIFI_MOD_CLASS_HE};
         const auto rusPerSubchannel =
             WifiRu::GetRusOfType(ruBw > MHz_t{20} ? ruBw : MHz_t{20}, ruType, mc);
         if ((m_channelWidth >= MHz_t{80}) && (ruIndex > 19))
@@ -835,7 +837,7 @@ WifiTxVector::DeriveRuAllocation(uint8_t p20Index) const
                                               : ((ruIndex - 1) * numSubchannelsForRu);
         NS_ABORT_IF(index >= Count20MHzSubchannels(m_channelWidth));
         auto ruAlloc = WifiRu::GetEqualizedRuAllocation(ruType, false, true, mc);
-        if (ruAllocations.at(index) != EMPTY_242_TONE_HE_RU)
+        if (ruAllocations.at(index) != emptyRu)
         {
             if (ruType == ruTypes.at(index))
             {
@@ -873,7 +875,7 @@ WifiTxVector::DeriveRuAllocation(uint8_t p20Index) const
         }
         for (std::size_t i = 0; i < numSubchannelsForRu; ++i)
         {
-            auto ruAllocNoUsers = HeRu::GetEqualizedRuAllocation(ruType, false, false);
+            auto ruAllocNoUsers = WifiRu::GetEqualizedRuAllocation(ruType, false, false, mc);
             ruTypes.at(index + i) = ruType;
             ruAllocations.at(index + i) =
                 (IsSigBCompression() || ((index + i) % 2) == ccIndex) ? ruAlloc : ruAllocNoUsers;

src/wifi/test/wifi-mac-ofdma-test.cc
@@ -305,22 +305,38 @@ TestMultiUserScheduler::ComputeWifiTxVector()
     RuType ruType;
     switch (static_cast<uint16_t>(bw.in_MHz()))
     {
-    case 20:
+    case 20: {
         ruType = RuType::RU_52_TONE;
-        m_txVector.SetRuAllocation({112}, 0);
+        const uint16_t ruAllocPer20 = IsEht(m_txVector.GetPreambleType()) ? 24 : 112;
+        m_txVector.SetRuAllocation({ruAllocPer20}, 0);
         break;
-    case 40:
+    }
+    case 40: {
         ruType = RuType::RU_106_TONE;
-        m_txVector.SetRuAllocation({96, 96}, 0);
+        const uint16_t ruAllocPer20 = IsEht(m_txVector.GetPreambleType()) ? 48 : 96;
+        m_txVector.SetRuAllocation({ruAllocPer20, ruAllocPer20}, 0);
         break;
-    case 80:
+    }
+    case 80: {
         ruType = RuType::RU_242_TONE;
-        m_txVector.SetRuAllocation({192, 192, 192, 192}, 0);
+        const uint16_t ruAllocPer20 = IsEht(m_txVector.GetPreambleType()) ? 64 : 112;
+        m_txVector.SetRuAllocation({ruAllocPer20, ruAllocPer20, ruAllocPer20, ruAllocPer20}, 0);
         break;
-    case 160:
+    }
+    case 160: {
         ruType = RuType::RU_484_TONE;
-        m_txVector.SetRuAllocation({200, 200, 200, 200, 200, 200, 200, 200}, 0);
+        const uint16_t ruAllocPer20 = IsEht(m_txVector.GetPreambleType()) ? 72 : 200;
+        m_txVector.SetRuAllocation({ruAllocPer20,
+                                    ruAllocPer20,
+                                    ruAllocPer20,
+                                    ruAllocPer20,
+                                    ruAllocPer20,
+                                    ruAllocPer20,
+                                    ruAllocPer20,
+                                    ruAllocPer20},
+                                   0);
         break;
+    }
     default:
         NS_ABORT_MSG("Unsupported channel width");
     }
@@ -330,12 +346,20 @@ TestMultiUserScheduler::ComputeWifiTxVector()
     for (auto& sta : staList)
     {
         auto index{ruIndex};
-        if ((bw == MHz_t{160}) && (ruIndex == 3))
+        if (!IsEht(m_txVector.GetPreambleType()) && (bw == MHz_t{160}) && (ruIndex >= 3))
         {
-            index = HeRu::GetIndexIn80MHzSegment(bw, ruType, ruIndex);
-            primary80 = HeRu::GetPrimary80MHzFlag(bw, ruType, ruIndex, 0);
+            index = ruIndex - 2;
+            primary80 = false;
         }
-        m_txVector.SetHeMuUserInfo(sta.first, {HeRu::RuSpec{ruType, index, primary80}, 11, 1});
+        if (IsEht(m_txVector.GetPreambleType()) && (bw > MHz_t{80}))
+        {
+            index = EhtRu::GetIndexIn80MHzSegment(bw, ruType, ruIndex);
+            primary80 = EhtRu::GetPrimaryFlags(bw, ruType, ruIndex, 0).second;
+        }
+        const auto ru = IsEht(m_txVector.GetPreambleType())
+                            ? WifiRu::RuSpec(EhtRu::RuSpec{ruType, index, true, primary80})
+                            : WifiRu::RuSpec(HeRu::RuSpec{ruType, index, primary80});
+        m_txVector.SetHeMuUserInfo(sta.first, {ru, 11, 1});
         ruIndex++;
     }
     m_txVector.SetSigBMode(VhtPhy::GetVhtMcs5());

404SkillNotFound/ns-3-dev

src/wifi/model/eht/eht-ppdu.h
@@ -151,6 +151,12 @@ class EhtPpdu : public HePpdu
                                     uint8_t ehtPpduType,
                                     std::optional<bool> isLow80MHz);
 
+  protected:
+    WifiRu::RuSpec GetRuSpec(std::size_t ruAllocIndex,
+                             MHz_u bw,
+                             RuType ruType,
+                             std::size_t phyIndex) const override;
+
   private:
     bool IsDlMu() const override;
     bool IsUlMu() const override;

src/wifi/model/he/he-phy.h
@@ -427,29 +427,30 @@ class HePhy : public VhtPhy
      */
     static Time GetSymbolDuration(Time guardInterval);
 
+    /// structure containing the information about the RU subcarriers to be able to converted to the
+    /// indices used by the Spectrum model
+    struct RuSubcarriersInfo
+    {
+        MHz_u bandWidth; ///< width of the band used for the OFDMA transmission. Must be a multiple
+                         ///< of 20 MHz
+        MHz_u guardBandwidth;                        ///< width of the guard band
+        const std::vector<MHz_u>& centerFrequencies; ///< center frequency of each segment
+        MHz_u totalWidth;                            ///< width of the operating channel
+        Hz_u subcarrierSpacing;                      ///< subcarrier spacing
+        WifiModulationClass mc;                      ///< modulation class used for the transmission
+        SubcarrierRange subcarrierRange;             ///< subcarrier range of the RU
+        uint8_t bandIndex{0}; ///< index (starting at 0) of the band within the operating channel
+    };
+
     /**
-     * @param bandWidth the width of the band used for the OFDMA transmission. Must be a multiple of
-     * 20 MHz
-     * @param guardBandwidth width of the guard band
-     * @param centerFrequencies the center frequency of each segment
-     * @param totalWidth the width of the operating channel
-     * @param subcarrierSpacing the subcarrier spacing
-     * @param subcarrierRange the subcarrier range of the RU
-     * @param bandIndex the index (starting at 0) of the band within the operating channel
-     * @return the converted subcarriers
-     *
      * This is a helper function to convert RU subcarriers, which are relative to the center
-     * frequency subcarrier, to the indexes used by the Spectrum model. The size of the returned
+     * frequency subcarrier, to the indices used by the Spectrum model. The size of the returned
      * vector corresponds to the number of segments covered by the RU.
+     *
+     * @param info the information about the RU subcarrier to convert
+     * @return the indices used by the Spectrum model for the provided RU subcarrier
      */
-    static std::vector<WifiSpectrumBandIndices> ConvertRuSubcarriers(
-        MHz_u bandWidth,
-        MHz_u guardBandwidth,
-        const std::vector<MHz_u>& centerFrequencies,
-        MHz_u totalWidth,
-        Hz_u subcarrierSpacing,
-        SubcarrierRange subcarrierRange,
-        uint8_t bandIndex = 0);
+    static std::vector<WifiSpectrumBandIndices> ConvertRuSubcarriers(const RuSubcarriersInfo& info);
 
   protected:
     PhyFieldRxStatus ProcessSig(Ptr<Event> event,

src/wifi/model/wifi-tx-vector.cc
@@ -691,6 +691,7 @@ WifiTxVector::GetInactiveSubchannels() const
 void
 WifiTxVector::SetCenter26ToneRuIndication(Center26ToneRuIndication center26ToneRuIndication)
 {
+    NS_ASSERT(GetModulationClass() == WIFI_MOD_CLASS_HE);
     if (IsDlMu())
     {
         NS_ASSERT(center26ToneRuIndication == DeriveCenter26ToneRuIndication());
@@ -701,7 +702,7 @@ WifiTxVector::SetCenter26ToneRuIndication(Center26ToneRuIndication center26ToneR
 std::optional<Center26ToneRuIndication>
 WifiTxVector::GetCenter26ToneRuIndication() const
 {
-    if (!IsDlMu() || (m_channelWidth < MHz_u{80}))
+    if ((GetModulationClass() != WIFI_MOD_CLASS_HE) || !IsDlMu() || (m_channelWidth < MHz_u{80}))
     {
         return std::nullopt;
     }
@@ -801,7 +802,10 @@ WifiTxVector::GetUserInfoMapOrderedByRus(uint8_t p20Index) const
 RuAllocation
 WifiTxVector::DeriveRuAllocation(uint8_t p20Index) const
 {
-    RuAllocation ruAllocations(Count20MHzSubchannels(m_channelWidth), EMPTY_242_TONE_HE_RU);
+    const auto mc = GetModulationClass();
+    const auto emptyRu =
+        (mc == WIFI_MOD_CLASS_HE) ? EMPTY_242_TONE_HE_RU : UNASSIGNED_242_TONE_EHT_RU;
+    RuAllocation ruAllocations(Count20MHzSubchannels(m_channelWidth), emptyRu);
     std::vector<RuType> ruTypes{};
     ruTypes.resize(ruAllocations.size());
     const auto& orderedMap = GetUserInfoMapOrderedByRus(p20Index);
@@ -817,8 +821,6 @@ WifiTxVector::DeriveRuAllocation(uint8_t p20Index) const
             continue;
         }
         const auto ruBw = WifiRu::GetBandwidth(ruType);
-        NS_ASSERT_MSG(WifiRu::IsHe(ru), "EHT RUs should not be used yet");
-        const auto mc{WIFI_MOD_CLASS_HE};
         const auto rusPerSubchannel =
             WifiRu::GetRusOfType(ruBw > MHz_u{20} ? ruBw : MHz_u{20}, ruType, mc);
         if ((m_channelWidth >= MHz_u{80}) && (ruIndex > 19))
@@ -835,7 +837,7 @@ WifiTxVector::DeriveRuAllocation(uint8_t p20Index) const
                                               : ((ruIndex - 1) * numSubchannelsForRu);
         NS_ABORT_IF(index >= Count20MHzSubchannels(m_channelWidth));
         auto ruAlloc = WifiRu::GetEqualizedRuAllocation(ruType, false, true, mc);
-        if (ruAllocations.at(index) != EMPTY_242_TONE_HE_RU)
+        if (ruAllocations.at(index) != emptyRu)
         {
             if (ruType == ruTypes.at(index))
             {
@@ -873,7 +875,7 @@ WifiTxVector::DeriveRuAllocation(uint8_t p20Index) const
         }
         for (std::size_t i = 0; i < numSubchannelsForRu; ++i)
         {
-            auto ruAllocNoUsers = HeRu::GetEqualizedRuAllocation(ruType, false, false);
+            auto ruAllocNoUsers = WifiRu::GetEqualizedRuAllocation(ruType, false, false, mc);
             ruTypes.at(index + i) = ruType;
             ruAllocations.at(index + i) =
                 (IsSigBCompression() || ((index + i) % 2) == ccIndex) ? ruAlloc : ruAllocNoUsers;

src/wifi/test/wifi-mac-ofdma-test.cc
@@ -305,22 +305,38 @@ TestMultiUserScheduler::ComputeWifiTxVector()
     RuType ruType;
     switch (static_cast<uint16_t>(bw))
     {
-    case 20:
+    case 20: {
         ruType = RuType::RU_52_TONE;
-        m_txVector.SetRuAllocation({112}, 0);
+        const uint16_t ruAllocPer20 = IsEht(m_txVector.GetPreambleType()) ? 24 : 112;
+        m_txVector.SetRuAllocation({ruAllocPer20}, 0);
         break;
-    case 40:
+    }
+    case 40: {
         ruType = RuType::RU_106_TONE;
-        m_txVector.SetRuAllocation({96, 96}, 0);
+        const uint16_t ruAllocPer20 = IsEht(m_txVector.GetPreambleType()) ? 48 : 96;
+        m_txVector.SetRuAllocation({ruAllocPer20, ruAllocPer20}, 0);
         break;
-    case 80:
+    }
+    case 80: {
         ruType = RuType::RU_242_TONE;
-        m_txVector.SetRuAllocation({192, 192, 192, 192}, 0);
+        const uint16_t ruAllocPer20 = IsEht(m_txVector.GetPreambleType()) ? 64 : 112;
+        m_txVector.SetRuAllocation({ruAllocPer20, ruAllocPer20, ruAllocPer20, ruAllocPer20}, 0);
         break;
-    case 160:
+    }
+    case 160: {
         ruType = RuType::RU_484_TONE;
-        m_txVector.SetRuAllocation({200, 200, 200, 200, 200, 200, 200, 200}, 0);
+        const uint16_t ruAllocPer20 = IsEht(m_txVector.GetPreambleType()) ? 72 : 200;
+        m_txVector.SetRuAllocation({ruAllocPer20,
+                                    ruAllocPer20,
+                                    ruAllocPer20,
+                                    ruAllocPer20,
+                                    ruAllocPer20,
+                                    ruAllocPer20,
+                                    ruAllocPer20,
+                                    ruAllocPer20},
+                                   0);
         break;
+    }
     default:
         NS_ABORT_MSG("Unsupported channel width");
     }
@@ -330,12 +346,20 @@ TestMultiUserScheduler::ComputeWifiTxVector()
     for (auto& sta : staList)
     {
         auto index{ruIndex};
-        if ((bw == MHz_u{160}) && (ruIndex == 3))
+        if (!IsEht(m_txVector.GetPreambleType()) && (bw == MHz_u{160}) && (ruIndex >= 3))
         {
-            index = HeRu::GetIndexIn80MHzSegment(bw, ruType, ruIndex);
-            primary80 = HeRu::GetPrimary80MHzFlag(bw, ruType, ruIndex, 0);
+            index = ruIndex - 2;
+            primary80 = false;
         }
-        m_txVector.SetHeMuUserInfo(sta.first, {HeRu::RuSpec{ruType, index, primary80}, 11, 1});
+        if (IsEht(m_txVector.GetPreambleType()) && (bw > MHz_u{80}))
+        {
+            index = EhtRu::GetIndexIn80MHzSegment(bw, ruType, ruIndex);
+            primary80 = EhtRu::GetPrimaryFlags(bw, ruType, ruIndex, 0).second;
+        }
+        const auto ru = IsEht(m_txVector.GetPreambleType())
+                            ? WifiRu::RuSpec(EhtRu::RuSpec{ruType, index, true, primary80})
+                            : WifiRu::RuSpec(HeRu::RuSpec{ruType, index, primary80});
+        m_txVector.SetHeMuUserInfo(sta.first, {ru, 11, 1});
         ruIndex++;
     }
     m_txVector.SetSigBMode(VhtPhy::GetVhtMcs5());