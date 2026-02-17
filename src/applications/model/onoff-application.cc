2978bb3478d74d6620663b1f6bb2797737b869d5
404SkillNotFound <shivang.upadhyay1@gmail.com>
2026-02-17 11:39:03 +0530

    modernize C-style casts: applications, internet, propagation, traffic-control

--- src/applications/model/onoff-application.cc
+++ src/applications/model/onoff-application.cc
@@ -271,7 +271,7 @@ OnOffApplication::SendPacket()
     }
 
     int actual = m_socket->Send(packet);
-    if ((unsigned)actual == m_pktSize)
+    if (static_cast<uint32_t>(actual) == m_pktSize)
     {
         m_txTrace(packet);
         m_totBytes += m_pktSize;
